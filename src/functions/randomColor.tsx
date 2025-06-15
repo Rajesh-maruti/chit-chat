80552e36ea133ec17280fc95531a396d7b5e322c
Rajesh <rajeshmaruti07@gmail.com>
2025-06-15 17:35:05 +0530

    Updates

--- src/functions/randomColor.tsx
+++
@@ -1,7 +0,0 @@
-export const randomRgbColor = () => {
-  let r = Math.floor(Math.random() * 256); // Random between 0-255
-  let g = Math.floor(Math.random() * 256); // Random between 0-255
-  let b = Math.floor(Math.random() * 256); // Random between 0-255
-  let a = (Math.random() * (0.6 - 0.4) + 0.4).toFixed(2); // Random between 0-255
-  return "rgb(" + r + "," + g + "," + b + "," + a + ")";
-};
